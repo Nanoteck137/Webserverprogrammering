bcc3c4a71f5678acad818d82b7cfbeb22f88761d
Nanoteck137 <patrik.millvik@gmail.com>
2018-10-25 09:01:24 +0200

    Uppgift 5: Färdig med registrerings koden

--- v43/submit.php
+++ v43/submit.php
@@ -1,12 +1,26 @@
 <?php
     $dbc_uppgift = mysqli_connect("localhost", "root", "", "uppgift");
 
-    $username = $_POST["username"];
-    $mail = $_POST["mail"];
-    $password = $_POST["password"];
+    if(isset($_POST["username"]) && isset($_POST["mail"]) && isset($_POST["password"])) {
+        $username = $_POST["username"];
+        $mail = $_POST["mail"];
+        $password = $_POST["password"];
 
+        $query = "INSERT INTO users(username, mail, password) VALUES ('$username', '$mail', '$password')";
 
-    $query = "INSERT INTO users(username, mail, password) VALUES ('$username', '$mail', '$password')";
-    mysqli_query($dbc_uppgift, $query);
-    mysqli_info($dbc_uppgift);
+        mysqli_query($dbc_uppgift, $query);
+        
+        $error = mysqli_errno($dbc_uppgift);
+        //NOTE: 1062 error code is Duplicate entry for unique columns
+        if ($error == 1062) {
+            echo "<h1>Error</h1>";
+            echo "<h3>Username or Mail is already in use</h3>";
+        }
+        else {
+            echo "<h1>$username is now registered</h1>";
+        }
+    }
+    else {
+        header("location: index.php");
+    }
 ?>
