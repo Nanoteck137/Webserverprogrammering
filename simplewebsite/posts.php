9ae6015ac39c47464e766aab0f3106dbeadb86b8
Nanoteck137 <patrik.millvik@gmail.com>
2018-12-09 17:28:41 +0100

    A search bar on the posts page

--- simplewebsite/posts.php
+++ simplewebsite/posts.php
@@ -23,6 +23,23 @@ require("include/common.php");
 
         <div id="content">
             <?php
+            if(isset($_GET["q"])) {
+            ?>
+                <div id="post-search-result">
+                    <p>Search Results For: <span><?= $_GET["q"]; ?></span></p>
+                </div>
+            <?php
+            }
+            ?>
+            <div id="post-header">
+                <form action="posts.php" method="get">
+                    <div id="post-search-bar">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="darkgray" d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
+                        <input type="text" name="q" placeholder="Search">
+                    </div>
+                </form>
+            </div>
+            <?php
             if(isset($_GET["view"])) {
                 $id = $_GET["view"];
                 $query = "SELECT posts.id, posts.post_name, posts.post_content, posts.post_date, users.user_name FROM posts JOIN users ON posts.post_user_id=users.id WHERE posts.id=$id;";
@@ -42,7 +59,16 @@ require("include/common.php");
             ?>
                 <div id="posts">
                     <?php
-                    $query = "SELECT posts.id, posts.post_name, posts.post_date, users.user_name FROM posts JOIN users ON posts.post_user_id=users.id;";
+
+                    $custom = "";
+
+                    //Search query
+                    if(isset($_GET["q"])) {
+                        $searchQuery = $_GET["q"];
+                        $custom = "WHERE posts.post_name LIKE '%$searchQuery%'";
+                    }
+
+                    $query = "SELECT posts.id, posts.post_name, posts.post_date, users.user_name FROM posts JOIN users ON posts.post_user_id=users.id $custom;";
                     $result = mysqli_query($dbc_website, $query);
 
                     while($row = mysqli_fetch_array($result)) {
