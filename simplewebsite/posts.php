0c35883f134af3350ea88cf6db786e6868dba6d2
Nanoteck137 <patrik.millvik@gmail.com>
2018-12-09 18:26:08 +0100

    Basic sorting with the posts

--- simplewebsite/posts.php
+++ simplewebsite/posts.php
@@ -35,8 +35,16 @@ require("include/common.php");
                 <form action="posts.php" method="get">
                     <div id="post-search-bar">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="darkgray" d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
-                        <input type="text" name="q" placeholder="Search">
+                        <input type="search" name="q" placeholder="Search" value="<?= isset($_GET["q"]) ? $_GET["q"] : "" ?>">
                     </div>
+
+                    <p>Newest</p>
+                    <input type="radio" name="sort" value="newest" <?= (isset($_GET["sort"]) && $_GET["sort"] === "newest") ? "checked" : "" ?>>
+
+                    <p>Oldest</p>
+                    <input type="radio" name="sort" value="oldest" <?= (isset($_GET["sort"]) && $_GET["sort"] === "oldest") ? "checked" : "" ?>>
+
+                    <button type="submit">Search</button>
                 </form>
             </div>
             <?php
@@ -45,8 +53,6 @@ require("include/common.php");
                 $query = "SELECT posts.id, posts.post_name, posts.post_content, posts.post_date, users.user_name FROM posts JOIN users ON posts.post_user_id=users.id WHERE posts.id=$id;";
                 $result = mysqli_query($dbc_website, $query);
                 $row = mysqli_fetch_array($result);
-
-                $d = new DateTime();
             ?>
                 <div id="view-post" class="card">
                     <p id="view-post-name"><?= $row["post_name"]?></p>
@@ -65,14 +71,27 @@ require("include/common.php");
                     //Search query
                     if(isset($_GET["q"])) {
                         $searchQuery = $_GET["q"];
-                        $custom = "WHERE posts.post_name LIKE '%$searchQuery%'";
+                        $custom = "WHERE posts.post_name LIKE '%$searchQuery%' ";
+                    }
+
+                    if(isset($_GET["sort"])) {
+                        $sort = $_GET["sort"];
+                        $order = "ORDER BY posts.post_date ";
+                        if($sort === "newest") {
+                            $order .= "DESC ";
+                        } else if($sort === "oldest") {
+                            $order .= "ASC ";
+                        } else {
+                            $order = "";
+                        }
+
+                        $custom .= $order;
                     }
 
                     $query = "SELECT posts.id, posts.post_name, posts.post_date, users.user_name FROM posts JOIN users ON posts.post_user_id=users.id $custom;";
                     $result = mysqli_query($dbc_website, $query);
 
                     while($row = mysqli_fetch_array($result)) {
-                        $d = new DateTime($row["post_date"]);
                     ?>
                         <a class="post card-hover" href="<?= $_SERVER["PHP_SELF"] . "?view=".$row["id"]; ?>">
                             <img id="post-img" src="image/test<?= rand(1, 2); ?>.jpg" alt="post-img">
