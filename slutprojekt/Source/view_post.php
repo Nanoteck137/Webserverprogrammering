6a0d875c3024446aab0c949335456257ce796e10
Nanoteck137 <patrik.millvik@gmail.com>
2019-05-06 12:04:32 +0200

    View post page

--- slutprojekt/Source/view_post.php
+++ slutprojekt/Source/view_post.php
@@ -9,15 +9,45 @@
 
 <head>
     <?php include "template/head.php" ?>
+    <link rel="stylesheet" href="css/pages/view_post.css">
 </head>
 
 <body>
     <div id="container">
         <?php include "template/header.php" ?>
         <main>
-            <p>Hello World</p>
+            <div id="view-post-author">
+                <a href="view_profile.php">Nanoteck137</a>
+                <p>2 hour ago</p>
+            </div>
+            <p id="view-post-title">Hello World</p>
 
-            <p>Woooh this is a post and woooh</p>
+            <p id="view-post-content">Woooh this is a post and woooh</p>
+            <div id="view-post-info">
+                <p><i class="fas fa-chevron-up"></i> 4 <span class="view-post-info-text">upvotes</span></p>
+                <p><i class="fas fa-comments"></i> 22 <span class="view-post-info-text">Comments</span></p>
+                <p><i class="fas fa-chevron-down"></i> 2 <span class="view-post-info-text">downvotes</span></p>
+            </div>
+
+            <div id="view-post-comments">
+
+                <?php
+            for($i = 0; $i < 22; $i++) 
+            {
+            ?>
+                <div class="view-post-comment">
+                    <div class="view-post-comment-author">
+                        <a href="view_profile.php">Nanoteck137</a>
+                        <p>1 hour ago</p>
+                    </div>
+
+                    <p class="view-post-commment-content">This sucks kill yourself</p>
+                </div>
+                <?php
+            }
+            ?>
+
+            </div>
         </main>
         <?php include "template/footer.php" ?>
     </div>
